67662272865cc225d8af67cb51626213a3854ab2
Rommer Alejandro Duque Andrade <rduque@banescoseguros.com>
2025-07-15 18:50:31 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -313,10 +313,9 @@ export default function DashboardPage() {
               </div>
             </div>
             <div className="relative grid grid-cols-2 grid-rows-2 gap-4 h-[500px]">
-              
               <div className="col-span-1 row-span-2 rounded-2xl overflow-hidden shadow-lg">
                   <Image
-                      src="https://images.unsplash.com/photo-1475503572774-15a45e5d60b9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxNnx8ZmFtaWxpYXxlbnwwfHx8fDE3NTI2MDUxMzd8MA&ixlib=rb-4.1.0&q=80&w=1080"
+                      src="https://images.unsplash.com/photo-1531983412531-1f49a365ffed?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxNHx8ZmFtaWxpYXxlbnwwfHx8fDE3NTI2MDUxMzd8MA&ixlib=rb-4.1.0&q=80&w=1080"
                       alt="Equipo de Banesco Seguros"
                       width={400}
                       height={600}
@@ -829,3 +828,4 @@ export default function DashboardPage() {
 
 
     
+

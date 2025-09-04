3d254b1ada72945866c7d85962b2a735d9971772
Rommer Alejandro Duque Andrade <rduque@banescoseguros.com>
2025-09-04 20:58:10 +0000

    creo que no hiciste ningún cambio

---
+++ src/next.config.ts
@@ -0,0 +1,26 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;

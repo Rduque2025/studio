233fa33fcf174e56c1003a7ed3a69f44825291c2
Rommer Alejandro Duque Andrade <rduque@banescoseguros.com>
2025-09-11 18:48:16 +0000

    Please update the image here to the newly chosen ones (_for element <Cou

--- src/app/dashboard/cursos/page.tsx
+++ src/app/dashboard/cursos/page.tsx
@@ -16,7 +16,7 @@ export default function CursosPage() {
   const categories = useMemo(() => {
     const uniqueCategories = [...new Set(mockCourses.map(course => course.category))];
     const categoryData: { [key: string]: { imageUrl: string; dataAiHint: string } } = {
-      "Google Workspace": { imageUrl: "https://images.unsplash.com/photo-1521737711867-e3b97375f902?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw1fHxtZWV0aW5nfGVufDB8fHx8fDE3NTMzMDY4NzN8MA&ixlib=rb-4.1.0&q=80&w=1080", dataAiHint: "team meeting" },
+      "Google Workspace": { imageUrl: "https://images.unsplash.com/photo-1592096304832-62463bfdc822?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw1fHxnb29nbGV8ZW58MHx8fHwxNzU3NTE5Njg2fDA&ixlib=rb-4.1.0&q=80&w=1080", dataAiHint: "google logo" },
       "Desarrollo": { imageUrl: "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxwcmVzZW50YXRpb258ZW58MHx8fHwxNzUzMzA3NDEzfDA&ixlib=rb-4.1.0&q=80&w=1080", dataAiHint: "business presentation" },
     };
     return uniqueCategories.map(cat => ({

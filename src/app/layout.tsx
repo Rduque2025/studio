569bbe6627422d814dc6ab2eb72e385bb1117802
Rommer Alejandro Duque Andrade <rduque@banescoseguros.com>
2025-09-19 13:46:19 +0000

    y si te proporciono una url de imagen podrías hacerlo? reemplazando el i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: 'Portal de Gestión - Banesco Seguros',
   description: 'Portal de Gestión Interna de Banesco Seguros',
+  icons: {
+    icon: 'https://spcdn.shortpixel.ai/spio/ret_img,q_cdnize,to_auto,s_webp:avif/banescointernacional.com/wp-content/uploads/2024/12/cropped-banescointernacional.png',
+  },
 };
 
 export default function RootLayout({

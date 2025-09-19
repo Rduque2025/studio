9039793536f4cdf1335195c378d55bf4a09d2900
Rommer Alejandro Duque Andrade <rduque@banescoseguros.com>
2025-09-19 13:50:46 +0000

    Claro, el ícono (favicon) no se muestra porque en las versiones reciente

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: 'Portal de Gestión - Banesco Seguros',
   description: 'Portal de Gestión Interna de Banesco Seguros',
+  icons: {
+    icon: 'https://spcdn.shortpixel.ai/spio/ret_img,q_cdnize,to_auto,s_webp:avif/banescointernacional.com/wp-content/uploads/2024/12/cropped-banescointernacional.png',
+  },
 };
 
 export default function RootLayout({
@@ -15,9 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="https://spcdn.shortpixel.ai/spio/ret_img,q_cdnize,to_auto,s_webp:avif/banescointernacional.com/wp-content/uploads/2024/12/cropped-banescointernacional.png" />
-      </head>
       {/* font-sans will now use 'Poppins' from Tailwind config */}
       <body className={`font-sans antialiased`}> 
         {children}
